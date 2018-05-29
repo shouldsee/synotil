98b0d6b3de37e5211bf7e2d16a1ad7691dc7f2ca
shouldsee <shouldsee@github.com>
2018-05-29 19:16:29 +0100

    tidy up

--- pipeline_rnaseq/preprocessor.py
+++ pipeline_rnaseq/preprocessor.py
@@ -9,99 +9,54 @@
 # Update: 29 May 2018. Feng@SLCU preprocessor.py
 
 
-def stop(msg):
-    print(msg)
-    sys.exit()
+import tempfile,subprocess
+import os, sys, datetime, glob, re
+import multiprocessing as mp
 
-def get_para(fname, kind):
-    ''' kind is either @ or %  '''
-    d = {}
-    f = open(fname)
-    var_list = []
-    count = 1
-    for line in f:
-        line = line.strip()
-        if line.startswith(kind):
-            if len(line) < 2:
-                stop('Not a valid line %s -- too short' % (line))
-            s = line[1:].strip() # skip @ or %
-            lst = s.split('=')
-            if len(lst) < 2:
-                stop('Error at line: %s' % (line))
-            if kind == '@':
-                name = 'sample_%02d' % (count)
-                count += 1                
-            else:
-                name = lst[0]
-            value = lst[1]
-            d[name] = value
-            var_list.append(name)
-    f.close()
-    return (d, var_list)
 
-def get_sample(fname):
-    return get_para(fname, '@')
-
-def get_parameter(fname):
-    return get_para(fname, '%')
-    
 def make_directory(dir):
     if not os.path.isdir(dir):
         try:
             os.makedirs(dir)
         except:
-            stop('Cannot make directory %s. STOP.' % (dir))
+            raise Exception('Cannot make directory %s. STOP.' % (dir))
         
-def common_substring(lst):
-
-    # check all elements has L00
-    sz = []
-    for x in lst:
-        sz.append(len(x))
-        if not ('L00' in x):
-            stop('%s must contain L00' % (x))
-
-    if len(set(sz)) != 1:
-        stop('Not all file names have equal length. \n%s' % ('\n'.join(lst)))
-
-    s = lst[0]
-    l = s.split('_')
-    t = []
-    for x in l:
-        if 'L00' in x:  # stop once encounter L00
-            break
-        t.append(x)
-    return '_'.join(t)
-
-def check_file(path):
-    assert os.path.exists(path),'%s does not exist.' % (path)
-
-def check_directory(path):
-    time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    if not os.path.isdir(path):
-        write_log('error.log.txt', '[%s] %s does not exist.' % (time_str, path))        
-        return 'PATH_INVALID'
-    if len(glob.glob(os.path.join(path, '*.gz'))) < 8: # a total of 8 fastq.gz files is expected
-        if len(glob.glob(os.path.join(path, '*R1_001.fastq'))) < 4 or len(glob.glob(os.path.join(path,'*R2_001.fastq'))) < 4:
-            write_log('error.log.txt', '[%s] %s not enough file.' % (time_str, path))
-            return 'FILE_LACK'
-    return ''
-
-
-import tempfile,subprocess
-import os, sys, datetime, glob, re
 
 # shellexec = os.system
 def shellexec(cmd):
-#     return os.system('/bin/bash -c `%s`'%cmd)
-#     cmd = '/bin/bash -c `%s`'%cmd
     print(cmd) 
     return os.system(cmd)
+#     return os.system('/bin/bash -c `%s`'%cmd)
+#     cmd = '/bin/bash -c `%s`'%cmd
+
 #     return subprocess.call(cmd,env=os.environ,cwd=os.getcwd(),
 #                           shell=True)
+
+def check_all_samples(d):
+    '''
+    Recursively walking a directory tree
+    TBM to return useful output
+    '''
+    for k in sorted(d.keys()):
+        rpath = d[k]
+        result = check_directory(rpath)
+        if result != '':
+            print('%s:%s [%s]' % (k, rpath, result))
+            rpath = rpath.rstrip('/')
+            if glob.glob(rpath) is []: # rpath does not exist or it is an incomplete path
+                path_lst = glob.glob(rpath + '*/')
+                assert len(path_lst) == 1,'Found multiple directories under %s'%rpath
+                rpath = path_lst[0]
+                d[k] = rpath.rstrip('/') # update rpath
+            
+def write_log(fname, s):
+    f = open(fname, 'a')
+    f.write(s + '\n')
+    f.close()
+
 def gnuPara(cmd,debug=0,ncore = 6):
     '''
-    Bad and does not wait for tasks to finish
+    [Deprecated] Bad and does not wait for tasks to finish
     '''
     tmp = tempfile.NamedTemporaryFile(delete=True) if not debug else open('temp.sh','w')
     with tmp as tmp:
@@ -113,9 +68,7 @@ def gnuPara(cmd,debug=0,ncore = 6):
             )
         )
     return E
-def worker(cmd):
-    return shellexec(cmd)
-import multiprocessing as mp
+
 def mp_para(f,lst,ncore = 6):
     p = mp.Pool(ncore)
     res = p.map_async(f,lst,)
@@ -216,26 +169,6 @@ def process_rna_sample(samplePATH, debug=0):
     shellexec(cmd) # CHANGE    
     pass
 
-
-def check_all_samples(d):
-    for k in sorted(d.keys()):
-        rpath = d[k]
-        result = check_directory(rpath)
-        if result != '':
-            print('%s:%s [%s]' % (k, rpath, result))
-            rpath = rpath.rstrip('/')
-            if glob.glob(rpath) == []: # rpath does not exist or it is an incomplete path
-                path_lst = glob.glob(rpath + '*')
-                if len(path_lst) == 1: # wildcard listing returns a unique path, use it
-                    rpath = path_lst[0]
-                    d[k] = rpath # update rpath
-                else:
-                    stop('STOP. Bad path.')
-            
-def write_log(fname, s):
-    f = open(fname, 'a')
-    f.write(s + '\n')
-    f.close()
 
 
 assert len(sys.argv) >= 2,'''
