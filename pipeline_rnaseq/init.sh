1d4659a1eb089c368dd16fffdaffae1770fb3f81
shouldsee <shouldsee@github.com>
2018-05-29 10:50:18 +0100

    []format

--- pipeline_rnaseq/init.sh
+++ pipeline_rnaseq/init.sh
@@ -1,32 +1,22 @@
 #!/usr/bin/env bash
 set -e
+
 ##### Initialising a working environment in current directory 
 SELF=${BASH_SOURCE[0]}
 ENVDIR=$PWD
 
+############################################
 ##### install binaries
 JARLIB=$PWD/jar
 mkdir -p $PWD/bin
 mkdir -p $JARLIB
-
-# pip install --user pyfaidx
-# fastqc is assumed installed
 
 ### Trimmomatic
 # must supply absolute paths
 TRIMDIR=/home/Program_NGS_sl-pw-srv01/Trimmomatic-0.32
 printf '#!/usr/bin/env bash \njava -jar $TRIMDIR/trimmomatic-0.32.jar "$@"'>$ENVDIR/bin/trimmomatic
 
-##### Prepare adapter seqs
-mkdir -p $PWD/adapters
-ln -sf $TRIMDIR/adapters/* adapters
-cd adapters
-if [ ! -e "TruSeq3-PE-all.fa" ]; then
-#     :
-# else
-    cat TruSeq3-PE*.fa >TruSeq3-PE-all.fa
-fi
-cd ..
+
 
 
 ### HISAT2
@@ -37,7 +27,28 @@ ln -sf /home/Program_NGS_sl-pw-srv01/picard-tools-1.103/*.jar $JARLIB
 ln -sf /home/Program_NGS_sl-pw-srv01/stringtie-1.3.3b.Linux_x86_64/stringtie $ENVDIR/bin
 
 
-##### Index genome.fa
+##### Assumed installed
+# fastqc
+# pip install --user pyfaidx
+##### Install binaries
+############################################
+
+
+
+############################################
+## ==== Preparing different FASTA files =====
+#### Prepare adapter fasta
+mkdir -p $PWD/adapters
+ln -sf $TRIMDIR/adapters/* adapters
+cd adapters
+if [ ! -e "TruSeq3-PE-all.fa" ]; then
+#     :
+# else
+    cat TruSeq3-PE*.fa >TruSeq3-PE-all.fa
+fi
+cd ..
+
+#### Index genome.fa
 mkdir -p $PWD/ref
 #ln -s /home/ref_genew/Brachypodium_Bd21_v3.1/assembly/Bdistachyon_314_v3.0.fa ref/genome.fa
 ln -sf /home/ref_genew/Brachypodium_Bd21_v3.1/* ref
@@ -47,3 +58,6 @@ faidx ref/genome.fa -i chromsizes > ref/genome.sizes
 
 cp -f ${SELF%/*}/activate.sh bin/activate
 cp -f ${SELF%/*}/*.sh bin/
+
+## ---- Preparing different FASTA files ----
+############################################
--- pipeline_rnaseq/parttest.sh
+++
@@ -1,5 +0,0 @@
-INDIR="${ENVDIR}/raw"
-read1=$INDIR/test_R1_.fastq
-read2=$INDIR/test_R2_.fastq
-pipeline_rnaseq.sh $read1 $read2 &>runlog
-
