dd1f8ecc078c782baed660198fb2cb9625772059
shouldsee <shouldsee@github.com>
2018-06-16 09:34:25 +0100

    [added] setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='synotil',
+      version='1.0',
+      description='Utility on Synology datasets',
+      author='Feng Geng',
+      author_email='shouldsee.gem@gmail.com',
+      url='none',
+      package_dir = {'synotil': 'pipeline_rnaseq'},
+      packages=['synotil', ],
+     )
