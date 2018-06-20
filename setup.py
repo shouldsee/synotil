e480fb35af2b1df434bf62944464a1ab4d84da2b
shouldsee <shouldsee@github.com>
2018-06-20 13:21:40 +0100

    [add] synotil setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='synotil',
       author='Feng Geng',
       author_email='shouldsee.gem@gmail.com',
       url='none',
-      package_dir = {'synotil': 'pipeline_rnaseq'},
-      packages=['synotil', ],
+      package_dir = {'': 'pipeline_rnaseq'},
+      packages=['', ],
      )
